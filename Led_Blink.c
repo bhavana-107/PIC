29f23bff08528445fff701983b491555579eb8e3
BHAVANA K <bhavana.kbhavanas@gmail.com>
2026-03-17 18:05:40 +0530

    Update Led_Blink.c

--- Led_Blink.c
+++ Led_Blink.c
@@ -17,3 +17,23 @@ void main(void) {
     }
     return;
 }
+
+
+//Led with button
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+
+void main(void) {
+    TRISC=0x08; //setting only RC3 as input other pins as outputs
+    while(1){    
+        while(RC3==0); //button at RC3
+        while(RC3==1);
+        PORTC=0x02;    //Led at RC1 
+        while(RC3==0);//
+        while(RC3==1);
+        PORTC=0x00;
+    }
+        
+    return;
+}
